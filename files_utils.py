ccbd4623047b96747186d2241c8ce4827b03099f
ael-code <tommy.ael@gmail.com>
2016-03-01 16:29:20 +0100

    bugfix: do not calculate size of links

--- files_utils.py
+++ files_utils.py
@@ -1,5 +1,8 @@
 import os
 import mimetypes
+from logging import getLogger
+
+logger = getLogger('pydirl')
 
 def get_file_size(path):
     return os.path.getsize(path)
@@ -11,7 +14,13 @@ def get_folder_size(path):
     for root, dirs, files in os.walk(path):
         for name in files:
             files_num += 1
-            size += os.path.getsize(os.path.join(root, name))
+            fPath = os.path.join(root, name)
+            #do not count size of link
+            if not os.path.islink(fPath):
+                try:
+                    size += os.path.getsize(fPath)
+                except OSError as e:
+                    logger.exception(e)
     return size, files_num
 
 def get_file_mimetype(path):
