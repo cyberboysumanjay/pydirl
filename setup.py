41835000adbe7fdcb6ce59e30cb36cb47d81676d
ael-code <tommy.ael@gmail.com>
2016-03-03 16:34:33 +0100

    setuptools stuff

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='pydirl',
-      version='0.1',
-      description='Quick file sharing solution',
-      license='GPLv3',
-      install_requires=[ 'Flask', 'flask-bootstrap']
-      py_modules = ['pydirl']
-      )
+setup(
+    name='pydirl',
+    version='0.1',
+    description='Quick file sharing solution',
+    license='GPLv3',
+    url='https://github.com/ael-code/pydirl',
+    install_requires=['Flask',
+                      'flask-bootstrap',
+                      'gevent',
+                      'click'],
+    packages = find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={'console_scripts': [ 'pydirl=pydirl.cli:pydirl']}
+)
