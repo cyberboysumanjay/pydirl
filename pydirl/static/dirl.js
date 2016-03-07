193b8d3ca824433f9fb0dd60d03f31ad5a9ff777
ael-code <tommy.ael@gmail.com>
2016-03-07 15:17:15 +0100

    added empty folder message

--- pydirl/static/dirl.js
+++ pydirl/static/dirl.js
@@ -1,9 +1,11 @@
 var suffix = ['B', 'KB','MB', 'GB', 'TB', 'PB']
+
 function populate_table(entries){
     var table = $('table').first();
 
     //dirs
-    dirs = entries['dirs']
+    dirs = entries['dirs'];
+    var n_dirs = 0;
     for( var dir in dirs ){
         if(dirs[dir]['size'] === null)
             h_size = null
@@ -14,17 +16,27 @@ function populate_table(entries){
                              'glyphicon glyphicon-folder-open',
                              size=h_size,
                              downloadUrl=dir+'/?download=true');
+        n_dirs += 1;
     }
 
     //files
     files = entries['files']
+    var n_files = 0;
     for( var file in files ){
         h_size = human_readable_size(files[file]['size'])
         append_table_element(file,
                              file,
                              'glyphicon glyphicon-unchecked',
                              h_size);
+        n_files += 1;
     }
+
+    if ((n_dirs + n_files) === 0){
+       console.log('The folder is empty folder');
+       show_message('This folder is empty');
+    }
+    console.log('Number of folders: '+ n_dirs);
+    console.log('Number of files: '+ n_files);
 }
 
 function append_table_element(name, url, icon_class, size, downloadUrl){
@@ -55,5 +67,13 @@ function human_readable_size(byte_size){
     return size+' '+suffix[suffix_idx];
 }
 
+function show_message(msg, alert_class){
+    if (alert_class === undefined)
+        alert_class = 'alert-warning';
+    var row = $('#template #alert-message').first().clone();
+    row.find('div .alert').first().addClass(alert_class);
+    row.find('p').first().text(msg);
+    $('table').replaceWith(row);
+}
 
 populate_table(entries);
